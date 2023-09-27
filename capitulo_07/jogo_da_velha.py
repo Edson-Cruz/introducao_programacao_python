97430a71d3455969cdabdbd15c3954b18355f8c6
Edson Cruz <edcruz.jr98@gmail.com>
2023-09-26 22:00:25 -0300

    Foi modificada a lógica para verificar quando o valor já tivesse sido selecionado

--- capitulo_07/jogo_da_velha.py
+++ capitulo_07/jogo_da_velha.py
@@ -29,105 +29,43 @@ print("\n 1 | 2 | 3 \n"
       " 7 | 8 | 9 \n")
 
 while True:
-    jogada_jogador_01 = int(input("Selecione a posição que deseja jogar: "))
-    if 1 <= jogada_jogador_01 <= 3: # Verificando a entrada de dados na primeira linha
+    jogada_jogador_01 = int(input("Jogador 1 - Selecione a posição que deseja jogar: "))
+    if (1 <= jogada_jogador_01 <= 3) or (jogada_jogador_01 not in numeros_jogados): # Verificando a entrada de dados na primeira linha
         if jogada_jogador_01 not in numeros_jogados: # Verificando se a posição desejada está livre para jogar
             numeros_jogados.append(jogada_jogador_01)
             linha_01[jogada_jogador_01 - 1] = marca_jogador_01
-            print(f"\n {linha_01[0]} | {linha_01[1]} | {linha_01[2]} \n"
-                  "---+---+---\n"
-                  f" {linha_02[0]} | {linha_02[1]} | {linha_02[2]} \n"
-                   "---+---+---\n"
-                  f" {linha_03[0]} | {linha_03[1]} | {linha_03[2]} \n")
+            print(f"\n {linha_01[0]} | {linha_01[1]} | {linha_01[2]}\n"
+                    "---+---+---\n"
+                    f" {linha_02[0]} | {linha_02[1]} | {linha_02[2]}\n"
+                    "---+---+---\n"
+                    f" {linha_03[0]} | {linha_03[1]} | {linha_03[2]}\n")
         else:
-            print("Posição já selecionada, jogue em outra posição!\n")
-            print(f"\n {linha_01[0]} | {linha_01[1]} | {linha_01[2]} \n"
-                  "---+---+---\n"
-                  f" {linha_02[0]} | {linha_02[1]} | {linha_02[2]} \n"
-                   "---+---+---\n"
-                  f" {linha_03[0]} | {linha_03[1]} | {linha_03[2]} \n")
-    elif 4 <= jogada_jogador_01 <= 6:
-        if jogada_jogador_01 not in numeros_jogados:
-            numeros_jogados.append(jogada_jogador_01)
-            linha_02[jogada_jogador_01 - 4] = marca_jogador_01
-            print(f"\n {linha_01[0]} | {linha_01[1]} | {linha_01[2]} \n"
-                   "---+---+---\n"
-                  f" {linha_02[0]} | {linha_02[1]} | {linha_02[2]} \n"
-                   "---+---+---\n"
-                  f" {linha_03[0]} | {linha_03[1]} | {linha_03[2]} \n")
-        else:
-            print("Posição já selecionada, jogue em outra posição!\n")
-            print(f"\n {linha_01[0]} | {linha_01[1]} | {linha_01[2]} \n"
-                  "---+---+---\n"
-                  f" {linha_02[0]} | {linha_02[1]} | {linha_02[2]} \n"
-                   "---+---+---\n"
-                  f" {linha_03[0]} | {linha_03[1]} | {linha_03[2]} \n")
-    elif 7 <= jogada_jogador_01 <= 9:
-        if jogada_jogador_01 not in numeros_jogados:
-            numeros_jogados.append(jogada_jogador_01)
-            linha_03[jogada_jogador_01 - 7] = marca_jogador_01
-            print(f"\n {linha_01[0]} | {linha_01[1]} | {linha_01[2]} \n"
-                   "---+---+---\n"
-                  f" {linha_02[0]} | {linha_02[1]} | {linha_02[2]} \n"
-                   "---+---+---\n"
-                  f" {linha_03[0]} | {linha_03[1]} | {linha_03[2]} \n")
-        else:
-            print("Posição já selecionada, jogue em outra posição!\n")
-            print(f"\n {linha_01[0]} | {linha_01[1]} | {linha_01[2]} \n"
-                  "---+---+---\n"
-                  f" {linha_02[0]} | {linha_02[1]} | {linha_02[2]} \n"
-                   "---+---+---\n"
-                  f" {linha_03[0]} | {linha_03[1]} | {linha_03[2]} \n")
-            
-    jogada_jogador_02 = int(input("Selecione a posição que deseja jogar: "))
-    if 1 <= jogada_jogador_02 <= 3: # Verificando a entrada de dados na primeira linha
-        if jogada_jogador_02 not in numeros_jogados: # Verificando se a posição desejada está livre para jogar
-            numeros_jogados.append(jogada_jogador_02)
-            linha_01[jogada_jogador_02 - 1] = marca_jogador_02
-            print(f"\n {linha_01[0]} | {linha_01[1]} | {linha_01[2]} \n"
-                  "---+---+---\n"
-                  f" {linha_02[0]} | {linha_02[1]} | {linha_02[2]} \n"
-                   "---+---+---\n"
-                  f" {linha_03[0]} | {linha_03[1]} | {linha_03[2]} \n")
-        else:
-            print("Posição já selecionada, jogue em outra posição!\n")
-            print(f"\n {linha_01[0]} | {linha_01[1]} | {linha_01[2]} \n"
-                  "---+---+---\n"
-                  f" {linha_02[0]} | {linha_02[1]} | {linha_02[2]} \n"
-                   "---+---+---\n"
-                  f" {linha_03[0]} | {linha_03[1]} | {linha_03[2]} \n")
-    elif 4 <= jogada_jogador_02 <= 6:
-        if jogada_jogador_02 not in numeros_jogados:
-            numeros_jogados.append(jogada_jogador_02)
-            linha_02[jogada_jogador_02 - 4] = marca_jogador_02
-            print(f"\n {linha_01[0]} | {linha_01[1]} | {linha_01[2]} \n"
-                   "---+---+---\n"
-                  f" {linha_02[0]} | {linha_02[1]} | {linha_02[2]} \n"
-                   "---+---+---\n"
-                  f" {linha_03[0]} | {linha_03[1]} | {linha_03[2]} \n")
-        else:
-            print("Posição já selecionada, jogue em outra posição!\n")
-            print(f"\n {linha_01[0]} | {linha_01[1]} | {linha_01[2]} \n"
-                  "---+---+---\n"
-                  f" {linha_02[0]} | {linha_02[1]} | {linha_02[2]} \n"
-                   "---+---+---\n"
-                  f" {linha_03[0]} | {linha_03[1]} | {linha_03[2]} \n")
-    elif 7 <= jogada_jogador_02 <= 9:
-        if jogada_jogador_02 not in numeros_jogados:
-            numeros_jogados.append(jogada_jogador_02)
-            linha_03[jogada_jogador_02 - 7] = marca_jogador_02
-            print(f"\n {linha_01[0]} | {linha_01[1]} | {linha_01[2]} \n"
-                   "---+---+---\n"
-                  f" {linha_02[0]} | {linha_02[1]} | {linha_02[2]} \n"
-                   "---+---+---\n"
-                  f" {linha_03[0]} | {linha_03[1]} | {linha_03[2]} \n")
-        else:
-            print("Posição já selecionada, jogue em outra posição!\n")
-            print(f"\n {linha_01[0]} | {linha_01[1]} | {linha_01[2]} \n"
-                  "---+---+---\n"
-                  f" {linha_02[0]} | {linha_02[1]} | {linha_02[2]} \n"
-                   "---+---+---\n"
-                  f" {linha_03[0]} | {linha_03[1]} | {linha_03[2]} \n")
-    else:
-        print(f"{jogada_jogador_01} - Número inválido, digite um número válido!")
-        break # Retirar esse break
+            while jogada_jogador_01 in numeros_jogados: # Verificando quando o campo já tiver sido selecionado
+                jogada_jogador_01 = int(input("Jogador 1 - Posição já selecionada! Selecione outra posição: "))
+                if (1 <= jogada_jogador_01 <= 3) and (jogada_jogador_01 not in numeros_jogados):
+                    numeros_jogados.append(jogada_jogador_01)
+                    linha_01[jogada_jogador_01 - 1] = marca_jogador_01
+                    print(f"\n {linha_01[0]} | {linha_01[1]} | {linha_01[2]}\n"
+                    "---+---+---\n"
+                    f" {linha_02[0]} | {linha_02[1]} | {linha_02[2]}\n"
+                    "---+---+---\n"
+                    f" {linha_03[0]} | {linha_03[1]} | {linha_03[2]}\n")
+                    break
+                elif (4 <= jogada_jogador_01 <= 6) and (jogada_jogador_01 not in numeros_jogados):
+                    numeros_jogados.append(jogada_jogador_01)
+                    linha_02[jogada_jogador_01 - 4] = marca_jogador_01
+                    print(f"\n {linha_01[0]} | {linha_01[1]} | {linha_01[2]}\n"
+                    "---+---+---\n"
+                    f" {linha_02[0]} | {linha_02[1]} | {linha_02[2]}\n"
+                    "---+---+---\n"
+                    f" {linha_03[0]} | {linha_03[1]} | {linha_03[2]}\n")
+                    break
+                elif (7 <= jogada_jogador_01 <= 9) and (jogada_jogador_01 not in numeros_jogados):
+                    numeros_jogados.append(jogada_jogador_01)
+                    linha_03[jogada_jogador_01 - 7] = marca_jogador_01
+                    print(f"\n {linha_01[0]} | {linha_01[1]} | {linha_01[2]}\n"
+                    "---+---+---\n"
+                    f" {linha_02[0]} | {linha_02[1]} | {linha_02[2]}\n"
+                    "---+---+---\n"
+                    f" {linha_03[0]} | {linha_03[1]} | {linha_03[2]}\n")
+                    break
